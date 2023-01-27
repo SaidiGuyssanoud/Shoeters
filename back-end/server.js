3dad25970258ad8c42d26b477872a5581807dd90
Saidi Guyssanoud <saidi.guyssanoud@gmail.com>
2023-01-27 12:26:01 +0100

    fetch data from DB

--- back-end/server.js
+++ back-end/server.js
@@ -1,33 +1,31 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mysql = require('promise-mysql')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mysql = require("promise-mysql");
+const cors = require("cors");
 
-const userRoutes = require('./routes/userRouter')
-const productRoutes = require('./routes/productRouter')
+const userRoutes = require("./routes/userRouter");
+const productRoutes = require("./routes/productRouter");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 const connectionOptions = {
-    host: process.env.DB_HOST,
-    database: process.env.DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    port: process.env.DB_PORT
-}
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  port: process.env.DB_PORT,
+};
 
 app.listen(port, () => {
-    console.log("Scooter, Écouteurs: le port 5000 est guetteur 🎶")
-})
+  console.log("Scooter, Écouteurs: le port 5000 est guetteur 🎶");
+});
 
-mysql.createConnection(connectionOptions)
-    .then(async (db) => {
-
-        userRoutes(app, db)
-        productRoutes(app, db)
-
-
-    })
+mysql.createConnection(connectionOptions).then(async (db) => {
+  userRoutes(app, db);
+  productRoutes(app, db);
+});
--- front-end/src/components/Accueil/AvailableShoes.jsx
+++ front-end/src/components/Accueil/AvailableShoes.jsx
@@ -1,83 +1,102 @@
 import React from 'react';
 import classes from "./AvailableShoes.css"
-const ShoesList = [
-    {
-        id: "1",
-        name: "Nike Dunk Low Retro ",
-        brand: "Nike",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTWoDh1GEHz0xNAIjxEyZMU_1UV-bSIDTQ9g&usqp=CAU ",
-        price: "99.99 ",
-        gender: "male",
-    },
-    {
-        id: "2",
-        name: "Puma",
-        brand: "Puma wild Rider layers",
-        image: "https://media.gqmagazine.fr/photos/60ace6ec59feb6bf2d96e9bb/master/w_1920,h_1280,c_limit/25052021_JAY_01.jpg",
-        price: "99.99",
-        gender: "male",
-    },
-    {
-        id: "3",
-        name: "Adidas Niteball",
-        brand: "Adidas",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAIRixflZv7TSILjVds0O6cm9ZsOeYLIcXPA&usqp=CAU ",
-        price: "99.99",
-        gender: "male",
-    },
+import { useState, useEffect } from 'react';
 
-    {
-        id: "1",
-        name: "Puma Slipstream",
-        brand: "Puma",
-        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/388634/03/sv01/fnd/EEA/fmt/png/Baskets-Slipstream-Suede-FS",
-        price: "89.99",
-        gender: "female",
-    },
-    {
-        id: "2",
-        name: "Nike Blazer Bordeaux",
-        brand: "Nike",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI62tRQBAOH7XVNRdhSFWOcgZ9INzdQkJBXrbd6xRP0UQXAVkrcIbUZpq6MJtNSwxS3WA&usqp=CAU",
-        price: "89.99",
-        gender: "female",
-    },
-    {
-        id: "3",
-        name: "Adidas Nmd Light Gray",
-        brand: "Adidas",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWmQWd4EDgpOsVJLhLDX7cnxXMeRoHm1ZxRg&usqp=CAU",
-        price: "89.99",
-        gender: "female",
-    },
+// const ShoesList = [
+//     {
+//         id: "1",
+//         name: "Nike Dunk Low Retro ",
+//         brand: "Nike",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTWoDh1GEHz0xNAIjxEyZMU_1UV-bSIDTQ9g&usqp=CAU ",
+//         price: "99.99 ",
+//         gender: "male",
+//     },
+//     {
+//         id: "2",
+//         name: "Puma",
+//         brand: "Puma wild Rider layers",
+//         image: "https://media.gqmagazine.fr/photos/60ace6ec59feb6bf2d96e9bb/master/w_1920,h_1280,c_limit/25052021_JAY_01.jpg",
+//         price: "99.99",
+//         gender: "male",
+//     },
+//     {
+//         id: "3",
+//         name: "Adidas Niteball",
+//         brand: "Adidas",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAIRixflZv7TSILjVds0O6cm9ZsOeYLIcXPA&usqp=CAU ",
+//         price: "99.99",
+//         gender: "male",
+//     },
+
+//     {
+//         id: "1",
+//         name: "Puma Slipstream",
+//         brand: "Puma",
+//         image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_450,h_450/global/388634/03/sv01/fnd/EEA/fmt/png/Baskets-Slipstream-Suede-FS",
+//         price: "89.99",
+//         gender: "female",
+//     },
+//     {
+//         id: "2",
+//         name: "Nike Blazer Bordeaux",
+//         brand: "Nike",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI62tRQBAOH7XVNRdhSFWOcgZ9INzdQkJBXrbd6xRP0UQXAVkrcIbUZpq6MJtNSwxS3WA&usqp=CAU",
+//         price: "89.99",
+//         gender: "female",
+//     },
+//     {
+//         id: "3",
+//         name: "Adidas Nmd Light Gray",
+//         brand: "Adidas",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWmQWd4EDgpOsVJLhLDX7cnxXMeRoHm1ZxRg&usqp=CAU",
+//         price: "89.99",
+//         gender: "female",
+//     },
     
-    {
-        id: "1",
-        name: "Nike Huarache Big Run",
-        brand: "Nike",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ98J4ZWpKfcCfxMArVtbWL6MUjlowNEwg_LQ&usqp=CAU",
-        price: "49.99",
-        categorie: "kid",
-    },
-    {
-        id: "2",
-        name: "Puma Rs-x Toys",
-        brand: "Puma",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRCdfefjY4_adVx87Mlel1fhCmXsJ5NUJ_nQ&usqp=CAU",
-        price: "49.99",
-        categorie: "kid",
-    },
-    {
-        id: "3",
-        name: " Adidas High Top Postmove",
-        brand: "Adidas",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPYXt_SGQ-1IQywi4I20lHtcybzh7BXnblA&usqp=CAU",
-        price: "49.99",
-        categorie: "kid",
-    },
-] 
-const AvailableShoes = () => {
-const AllProducts = ShoesList.map(shoes => <li>{shoes.name}</li>)
+//     {
+//         id: "1",
+//         name: "Nike Huarache Big Run",
+//         brand: "Nike",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ98J4ZWpKfcCfxMArVtbWL6MUjlowNEwg_LQ&usqp=CAU",
+//         price: "49.99",
+//         categorie: "kid",
+//     },
+//     {
+//         id: "2",
+//         name: "Puma Rs-x Toys",
+//         brand: "Puma",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRCdfefjY4_adVx87Mlel1fhCmXsJ5NUJ_nQ&usqp=CAU",
+//         price: "49.99",
+//         categorie: "kid",
+//     },
+//     {
+//         id: "3",
+//         name: " Adidas High Top Postmove",
+//         brand: "Adidas",
+//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPYXt_SGQ-1IQywi4I20lHtcybzh7BXnblA&usqp=CAU",
+//         price: "49.99",
+//         categorie: "kid",
+//     },
+// ] 
+function AvailableShoes(){
+    const [shoes, setShoes] = useState([])
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/products");
+            const responseData = await response.json();
+            console.log(responseData);
+            setShoes(responseData);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+const AllProducts = shoes.map(shoe => <li>{shoe.product_name}</li>)
 
     return (
         <section className={classes.shoes}>
@@ -86,4 +105,4 @@ const AllProducts = ShoesList.map(shoes => <li>{shoes.name}</li>)
     )
 }
 
-export default AvailableShoes;
+export default AvailableShoes
